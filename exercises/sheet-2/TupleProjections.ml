1d15000d8638d0a68dd5cc3a20943dd3d9559ec6
Domai <github@domai-tb.com>
2024-05-03 22:04:10 +0200

    tuple shit

--- exercises/sheet-2/TupleProjections.ml
+++ exercises/sheet-2/TupleProjections.ml
@@ -3,16 +3,12 @@
 (1 point) Declare the functions fst : (int * int) -> int and snd : (int * int) -> int which "extract" the first and second component of a tuple respectively.
 
 *)
-let fst _ = (* Fill in here *)
-;;
-
-let snd _ = (* Fill in here *)
-;;
+let fst (x,y:int*int):int = x
+let snd (x,y:int*int):int = y
 
 (*
   
 (1 point) Declare a function proj : (int * (char * bool * string) * (unit * float)) -> char. Use the type signature as guidance to "infer" a sensible implementation of proj which does not return a constant.
 
 *)
-let proj _ = (* Fill in here *)
-;;
+let proj (a,(b,c,d),(e,f):int*(char*bool*string)*(unit*float)):char = (* fill out *)
